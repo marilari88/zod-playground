5d64893c45698e8302e73ae22e1c82c21d13449e
Marco Ilari <marilari88@gmail.com>
2024-04-20 13:29:42 +0200

    ⬆️ Upgrade deps

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
-import {MantineProvider} from '@mantine/core'
+import {MantineProvider, createTheme} from '@mantine/core'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+import '@mantine/core/styles.css'
+
+const theme = createTheme({
+  fontFamily: 'Fira Code, monospace',
+})
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <MantineProvider withGlobalStyles withNormalizeCSS>
+    <MantineProvider theme={theme}>
       <App />
     </MantineProvider>
   </React.StrictMode>,
