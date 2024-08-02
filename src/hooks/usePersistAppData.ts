6398c5f9f271d0d178f11d42e3c69d613ac4599e
giacomo.cerquone <cerquone@gmail.com>
2024-08-03 01:40:02 +0200

    feat: save data when tab gets closed

--- src/hooks/usePersistAppData.ts
+++ src/hooks/usePersistAppData.ts
@@ -6,9 +6,16 @@ const usePersistAppData = (data: AppData) => {
   const timeoutRef = useRef<number | null>(null)
 
   useEffect(() => {
-    timeoutRef.current = setTimeout(() => {
+    const saveData = () => {
       localStorage.setItem(STORAGE_KEY, JSON.stringify(data))
+    }
+
+    timeoutRef.current = setTimeout(() => {
+      saveData()
     }, 1000)
+
+    // This saves the data if the user close the tab within the 1 second timeframe
+    window.addEventListener('beforeunload', saveData)
 
     return () => {
       if (timeoutRef.current) {
