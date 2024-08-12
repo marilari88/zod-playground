3a12a698f54f2e6f62f4efeb92c621fd3a12fd47
Davide Conti <dconti.dev@gmail.com>
2024-08-12 22:48:41 +0200

    Rename test folder and npm test commands
    Co-authored-by: Marco Ilari <marilari88@gmail.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ import {defineConfig, devices} from '@playwright/test'
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: './test',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
