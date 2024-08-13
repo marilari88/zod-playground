f228e4374637d2419274a8073eb59daccfeeded2
Davide Conti <dconti.dev@gmail.com>
2024-08-13 12:48:46 +0200

    Refactor to rename "test" folder to "tests"
    Co-authored-by: Marco Ilari <marilari88@gmail.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ import {defineConfig, devices} from '@playwright/test'
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './test',
+  testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
