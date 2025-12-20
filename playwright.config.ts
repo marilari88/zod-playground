f79a241bf65da5586602d38104446ff4c001263f
Davide Conti <dconti.dev@gmail.com>
2025-12-20 17:43:46 +0100

    Remove retries for playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
