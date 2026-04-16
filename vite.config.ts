654674d66ebdf3ea7013b9548e87ab396903afdc
Abdalrhman Almarakeby <contact@abdalrhman.me>
2026-04-16 12:20:24 +0200

    chore: install and configure React Compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import react from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import react, {reactCompilerPreset} from '@vitejs/plugin-react'
 import {defineConfig} from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
+  ],
   build: {
     target: 'es2022',
   },
